73daf0f1a976f9abda11af50b40e291d2ee02ece
Hexzilla <hexzilla256@gmail.com>
2023-06-13 11:49:21 -0700

    add accordian

--- src/sections/dashboard/wallet/WalletView.tsx
+++ src/sections/dashboard/wallet/WalletView.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Card, Chip, Stack, Typography,Container, Grid, Button } from '@mui/material';
+import { Box, Card, Chip, Stack, Typography, Container, Grid, Button, AccordionDetails, AccordionSummary, Accordion } from '@mui/material';
 
 import Ethereum from '@components/icons/Ethereum';
 import CriticalChip from '@components/chips/CriticalChip';
@@ -15,6 +15,8 @@ import Down from '@components/icons/Down';
 import DoubleArrow from '@components/icons/DoubleArrow';
 import Help from '@components/icons/Help';
 import Lock from '@components/icons/Lock';
+import Iconify from '@components/iconify';
+import accordion from 'src/pages/components/mui/accordion';
 
 type Props = {
   onClosed?: VoidFunction;
@@ -26,11 +28,17 @@ const warnings: String[] = new Array(3).fill(0).map(
 );
 
 const buttonSX = {
-  width:'100%', backgroundColor:'black', marginTop:'12px',
+  width: '100%', backgroundColor: 'black', marginTop: '12px',
   "&:hover": {
     backgroundColor: "#2965FF",
   },
 };
+
+const accordionSX = {
+  "&:before": {
+    display: "none"
+  }
+}
 
 export default function WalletView({ onClosed }: Props) {
   return (
@@ -43,7 +51,7 @@ export default function WalletView({ onClosed }: Props) {
         justifyContent="space-between"
       >
         <Stack direction="row" alignItems="center">
-          <Transaction/>
+          <Transaction />
           <Typography>Medium Rist</Typography>
         </Stack>
         <Stack direction="column" spacing={1}>
@@ -51,8 +59,8 @@ export default function WalletView({ onClosed }: Props) {
         </Stack>
         <Stack direction="row" spacing={1} alignItems="center">
           <Typography>Wallet1</Typography>
-          <Box sx={{cursor:'pointer'}} onClick = {onClosed}>
-            <Close/>
+          <Box sx={{ cursor: 'pointer' }} onClick={onClosed}>
+            <Close />
           </Box>
         </Stack>
       </Stack>
@@ -74,7 +82,7 @@ export default function WalletView({ onClosed }: Props) {
           </Stack>
         </Stack>
       </Card>
-      <Card sx={{marginTop:'24px'}}>
+      {/* <Card sx={{marginTop:'24px'}}>
         <Stack
           direction="row"
           spacing={2}
@@ -107,12 +115,43 @@ export default function WalletView({ onClosed }: Props) {
             </Stack>
           </Card>
       ))}
-      </Card>
-      <Button href="/" size="large" variant="contained" 
+      </Card> */}
+      <Accordion sx={accordionSX}>
+        <AccordionSummary expandIcon={<Iconify icon="eva:arrow-ios-downward-fill" />}>
+          {
+            <Stack direction="row" spacing={3} justifyContent="space-between" alignItems="center">
+              <Alert />
+              <Typography sx={{ color: 'critical.main' }}>Critical Risks</Typography>
+            </Stack>
+          }
+        </AccordionSummary>
+        <AccordionDetails>
+          {warnings.map((row, index) => (
+            <Card sx={{ margin: '16px 30px' }} key={index}>
+              <Stack
+                direction="row"
+                spacing={2}
+                alignItems="center"
+                justifyContent="space-between"
+                sx={{ padding: '4px 4px' }}
+              >
+                <Stack direction="row" spacing={3} alignItems="center">
+                  <Alert />
+                  <Stack direction="column" spacing={1}>
+                    <Typography>Danger</Typography>
+                    <Typography color="grey">This is the danger first we flagged</Typography>
+                  </Stack>
+                </Stack>
+              </Stack>
+            </Card>
+          ))}
+        </AccordionDetails>
+      </Accordion>
+      <Button href="/" size="large" variant="contained"
         sx={buttonSX}>
         Go to Home
       </Button>
-      <Card sx={{marginTop:'12px'}}>
+      <Card sx={{ marginTop: '12px' }}>
         <Stack
           direction="row"
           spacing={2}
@@ -127,7 +166,7 @@ export default function WalletView({ onClosed }: Props) {
       </Card>
       <Stack
         alignItems="center"
-        sx={{ padding: '10px 20px', marginTop:"12px" }}
+        sx={{ padding: '10px 20px', marginTop: "12px" }}
       >
         <Stack
           direction="row"
