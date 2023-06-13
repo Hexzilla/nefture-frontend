a26456caf66c63c0b586d74cc78004e8e58f3ca6
Hexzilla <hexzilla256@gmail.com>
2023-06-13 21:44:19 +0900

    fix: type

--- src/components/confirm-dialog/types.ts
+++ src/components/confirm-dialog/types.ts
@@ -5,7 +5,7 @@ import { DialogProps } from '@mui/material';
 
 export interface ConfirmDialogProps extends Omit<DialogProps, 'title' | 'content'> {
   title: React.ReactNode;
-  content?: React.ReactNode | string;
+  content?: React.ReactNode;
   action: React.ReactNode;
   open: boolean;
   onClose: VoidFunction;
