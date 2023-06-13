d03c6da3fcc49c641abe4276b598fb4e290cdf2d
Hexzilla <hexzilla256@gmail.com>
2023-06-13 21:40:42 +0900

    delete pages

--- src/components/confirm-dialog/types.ts
+++ src/components/confirm-dialog/types.ts
@@ -3,7 +3,7 @@ import { DialogProps } from '@mui/material';
 
 // ----------------------------------------------------------------------
 
-export interface ConfirmDialogProps extends Omit<DialogProps, 'title'> {
+export interface ConfirmDialogProps extends Omit<DialogProps, 'title' | 'content'> {
   title: React.ReactNode;
   content?: React.ReactNode | string;
   action: React.ReactNode;
