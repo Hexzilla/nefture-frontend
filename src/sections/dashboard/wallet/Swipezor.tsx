91cfc108c8c00504180e69a6f59d6b84792dc116
Hexzilla <hexzilla256@gmail.com>
2023-06-17 09:26:38 +0900

    fix: build error

--- src/sections/dashboard/wallet/Swipezor.tsx
+++ src/sections/dashboard/wallet/Swipezor.tsx
@@ -1,7 +1,5 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef } from 'react';
 import { useSwipeable } from 'react-swipeable';
-// import DoubleArrow from '@components/icons/DoubleArrow';
-// import Lock from '@components/icons/Lock';
 
 function findLeft(element: any) {
   var rec = element.getBoundingClientRect();
@@ -21,11 +19,10 @@ function Swipezor({
   minSwipeVelocity = 0.6,
   caret = null,
   arrowVisible,
-}) {
+}: any) {
   const [overlayWidth, setOverlayWidth] = useState(40);
   const [swipeComplete, setSwipeComplete] = useState(false);
-  const [arrowVisibled, setArrowVisibled] = useState(arrowVisible);
-  const buttonRef = useRef();
+  const buttonRef = useRef<any>();
 
   useEffect(() => {
     if (reset) {
@@ -36,64 +33,72 @@ function Swipezor({
 
   const handlers = useSwipeable({
     onSwipedRight: (data) => {
-      if (swipeComplete)
-        return;
+      if (swipeComplete || !buttonRef.current) return;
+
+      const event: any = data.event;
       const butWidth = buttonRef.current.offsetWidth;
       if (data.velocity > minSwipeVelocity) {
         setOverlayWidth(butWidth);
         setSwipeComplete(true);
         setTimeout(() => onSwipeDone(), 100);
-      }
-      else {
+      } else {
         const offsetLeft = findLeft(buttonRef.current);
         const startPos = Math.abs(data.initial[0] - offsetLeft);
-        if (startPos <= 100 &&
-          (data.event.type === "touchend" ? data.event.changedTouches[0].clientX - offsetLeft : data.event.offsetX) > minSwipeWidth * butWidth
+        if (
+          startPos <= 100 &&
+          (data.event.type === 'touchend'
+            ? event.changedTouches[0].clientX - offsetLeft
+            : event.offsetX) >
+            minSwipeWidth * butWidth
         ) {
           setOverlayWidth(butWidth);
           setSwipeComplete(true);
           setTimeout(() => onSwipeDone(), 100);
-        }
-        else
-          setOverlayWidth(40);
+        } else setOverlayWidth(40);
       }
     },
     onSwiping: (data) => {
-      if (swipeComplete)
-        return;
+      if (swipeComplete) return;
+      
+      const event: any = data.event;
       const offsetLeft = findLeft(buttonRef.current);
       const startPos = Math.abs(data.initial[0] - offsetLeft);
       if (startPos <= 100) {
-        if (data.event.type && data.event.type === "touchmove")
-          setOverlayWidth(data.event.touches[0].clientX - offsetLeft);
-        else
-          setOverlayWidth(data.event.offsetX);
+        if (data.event.type && data.event.type === 'touchmove')
+          setOverlayWidth(event.touches[0].clientX - offsetLeft);
+        else setOverlayWidth(event.offsetX);
       }
-
     },
     delta,
     trackMouse: true,
-    preventDefaultTouchmoveEvent: true
   });
 
   return (
-    <div className={`swipezor-but ${classList}`} {...handlers} ref={(t) => {
-      handlers.ref(t);
-      buttonRef.current = t;
-    }}>
+    <div
+      className={`swipezor-but ${classList}`}
+      {...handlers}
+      ref={(t) => {
+        handlers.ref(t);
+        buttonRef.current = t;
+      }}
+    >
       <div className={`swipezor-overlay ${overlayClassList}`} style={{ width: overlayWidth }}>
         <div className="swipezor-overlay-wrapper">
           <div className={`swipezor-caret-wrapper ${caretClassList}${arrowVisible}`}>
-            {(caret || arrowVisible=="hidden") ? caret : <img src={'/assets/icons/arrow.png'} alt="caret" style={{ maxWidth: '100%' }} />}
+            {caret || arrowVisible == 'hidden' ? (
+              caret
+            ) : (
+              <img src={'/assets/icons/arrow.png'} alt="caret" style={{ maxWidth: '100%' }} />
+            )}
           </div>
-          <div className="swipezor-overlay-txt" style={{backgroundColor:'green'}}>
+          <div className="swipezor-overlay-txt" style={{ backgroundColor: 'green' }}>
             {overlayText}
           </div>
         </div>
       </div>
       {mainText}
     </div>
-  )
+  );
 }
 
 export default Swipezor;
