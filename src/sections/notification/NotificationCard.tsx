eda8b0f3d8f8343b5a95648e95e8a66d53fc079e
Hexzilla <hexzilla256@gmail.com>
2023-06-20 19:43:22 -0400

    feedback-card design to change

--- src/sections/notification/NotificationCard.tsx
+++ src/sections/notification/NotificationCard.tsx
@@ -1,46 +1,84 @@
 import { Card, Stack, Switch, Typography } from '@mui/material';
-
+import { DM_Mono } from 'next/font/google';
 
 type Props = {
   title: String;
   type: number;
 };
 
-const defaultStyle = [{
-  backgroundImage: 'linear-gradient(to right, #0EAE88, #1350C7)',
-  WebkitBackgroundClip: 'text',
-  MozBackgroundClip: 'text',
-  WebkitTextFillColor: 'transparent',
-  display: 'inline-block',
-},{
-  color:'#1350C7',
-  margin:'0.2em'
-}];
+const proStyle = [
+  {
+    backgroundImage: 'linear-gradient(to right, #0EAE88 100%, #1350C7)',
+    WebkitBackgroundClip: 'text',
+    MozBackgroundClip: 'text',
+    WebkitTextFillColor: 'transparent',
+    display: 'inline-block',
+  },
+  {
+    color: '#1350C7',
+    margin: '0.2em',
+  },
+];
+
+const defaultStyle = [
+  {
+    backgroundImage: 'linear-gradient(to right, #0EAE88 40%, #1350C7)',
+    WebkitBackgroundClip: 'text',
+    MozBackgroundClip: 'text',
+    WebkitTextFillColor: 'transparent',
+    display: 'inline-block',
+  },
+  {
+    color: '#1350C7',
+    margin: '0.2em',
+  },
+];
+
 const stackStyle = {
   spacing: 2,
   alignItems: 'center',
   justifyContent: 'space-between',
   marginTop: '1em',
-  marginBottom: '1em',
+  marginBottom: '0.5em',
 };
 
-export default function NotificationCard({ title,type }: Props) {
+const switchStyle = {
+  width: '66px',
+  height: '42px',
+  '.MuiSwitch-thumb': { width: '18px', height: '18px' },
+};
+
+const dmMono = DM_Mono({
+  weight: '400',
+  subsets: ['latin'],
+});
+
+export default function NotificationCard({ title, type }: Props) {
   return (
     <Card sx={{ padding: '1em' }}>
       <Stack direction="row" sx={stackStyle}>
         <Typography style={{}}>{title}</Typography>
-        {type==0&&<Switch defaultChecked color="success" />}
-        {type==1&&<Switch defaultChecked color="primary" />}
+        {type == 0 && <Switch defaultChecked color="success" sx={switchStyle} />}
+        {type == 1 && <Switch defaultChecked color="primary" sx={switchStyle} />}
       </Stack>
-      <Typography style={defaultStyle[type]}>PRO</Typography>
-      <Typography mt={1} color={'gray'}>
+      <Typography style={proStyle[type]}>PRO</Typography>
+      <Typography mt={1} color={'gray'} sx={{ marginTop: '1.5em', marginBottom: '1em', fontSize:'10px' }} className={dmMono.className}>
         This alerts tracks all your approvals over time.
       </Typography>
       <Stack direction="row" sx={stackStyle}>
         <Typography style={defaultStyle[type]}>See example</Typography>
-        {type==0&&<Card sx={{ padding: '0.5em', textAlign: 'center', borderRadius: 1 }}>
-          <Typography>Included with RPC</Typography>
-        </Card>}
+        {type == 0 && (
+          <Card
+            sx={{
+              padding: '0.5em',
+              textAlign: 'center',
+              borderRadius: 2,
+              backgroundColor: 'primary.buttonColor',
+            }}
+          >
+            <Typography>Included with RPC</Typography>
+          </Card>
+        )}
       </Stack>
     </Card>
   );
