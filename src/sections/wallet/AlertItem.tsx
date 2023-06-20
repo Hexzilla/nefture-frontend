ce016ce48afac95a905bc5a3671c7cadf1fabb92
Hexzilla <hexzilla256@gmail.com>
2023-06-19 20:59:20 -0400

    wallet table completion in desktop mode

--- src/sections/wallet/AlertItem.tsx
+++ src/sections/wallet/AlertItem.tsx
@@ -1,4 +1,4 @@
-import { Switch, Typography } from '@mui/material';
+import { Switch, Typography, Stack } from '@mui/material';
 
 import HelpWhite from '@components/icons/HelpWhite';
 import { WalletStatus } from './types';
@@ -10,10 +10,10 @@ type Props = {
 export default function AlertItem({ title }: Props) {
 
   return (
-    <>
+    <Stack direction="row" spacing={3} style={{alignItems:'center'}}>
       <HelpWhite />
       <Typography>{title}</Typography>
       <Switch defaultChecked={true} />
-    </>
+    </Stack>
   );
 }
--- src/sections/wallet/WalletItem.tsx
+++ src/sections/wallet/WalletItem.tsx
@@ -33,39 +33,58 @@ export default function WalletItem({ item }: Props) {
     position: 'relative',
   };
   const COLORS = ['error', 'warning', 'success'] as const;
-  
+
   return (
     <Card sx={styles}>
       <Stack
         direction="row"
         spacing={2}
         alignItems="center"
-        justifyContent="start"
+        justifyContent="space-between"
         sx={{ padding: '10px 20px', cursor: 'pointer' }}
       >
-        <EthereumIcon />
-        <Typography>Received at 12:56</Typography>
-        <PencilGray />
-        <Typography>0xebC73...8D0B2</Typography>
-        {item.status == 0 && (
-          <Card>
-            <Typography m={1.5}>Launch Check</Typography>
-          </Card>
-        )}
-        {item.status != 0 && (
-          <>
-          <Typography>{item.statusTitle}</Typography>
-            <Box sx={circleStyle}>
-              <CircularProgress color={COLORS[item.status-1]} variant="determinate" value={95} size={48} />
-              <Typography sx={{ position: 'absolute', top: '30%', left: '31%' }}>97</Typography>
-            </Box>
-          </>
-        )}
-        <AlertItem title='Weekly Reports'/>
-        <AlertItem title='Real-time Alerts'/>
-          <Card sx={{borderColor:'primary.main'}}>
+        <Stack direction="row" spacing={3} alignItems={'center'}>
+          <EthereumIcon />
+          <Typography>Received at 12:56</Typography>
+          <PencilGray />
+        </Stack>
+        <Stack direction="row" spacing={3} alignItems={'center'}>
+          <Typography>0xebC73...8D0B2</Typography>
+        </Stack>
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          spacing={3}
+          style={{ minWidth: '140px' }}
+          alignItems={'center'}
+        >
+          {item.status == 0 && (
+            <Card>
+              <Typography m={1.5}>Launch Check</Typography>
+            </Card>
+          )}
+          {item.status != 0 && (
+            <>
+              <Typography>{item.statusTitle}</Typography>
+              <Box sx={circleStyle}>
+                <CircularProgress
+                  color={COLORS[item.status - 1]}
+                  variant="determinate"
+                  value={95}
+                  size={48}
+                />
+                <Typography sx={{ position: 'absolute', top: '30%', left: '31%' }}>97</Typography>
+              </Box>
+            </>
+          )}
+        </Stack>
+        <AlertItem title="Weekly Reports" />
+        <AlertItem title="Real-time Alerts" />
+        <Stack direction="row" spacing={3} alignItems={'center'}>
+          <Card sx={{ borderColor: 'primary.main' }}>
             <Typography m={1.5}>Wallet 1</Typography>
           </Card>
+        </Stack>
       </Stack>
     </Card>
   );
